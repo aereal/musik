3e18b5fcf0e564cbb4cb595ea26d3f55c886f6d2
aereal <aereal@aereal.org>
2019-08-23 17:12:34 +0900

    Interval describe methdos

--- src/domain/models/interval.test.ts
+++ src/domain/models/interval.test.ts
@@ -7,6 +7,8 @@ describe("Interval", () => {
   const c1 = new Pitch(PitchClass.C, 1);
   const d0 = new Pitch(PitchClass.D, 0);
   const d1 = new Pitch(PitchClass.D, 1);
+  const e0 = new Pitch(PitchClass.E, 0);
+  const es0 = new Pitch(PitchClass.Es, 0);
 
   test("value", () => {
     expect(Interval.of(c0, c0)).toEqual(new Interval(0));
@@ -20,5 +22,32 @@ describe("Interval", () => {
   test("simplify", () => {
     expect(Interval.of(c0, c1).simplify()).toEqual(new Interval(0));
     expect(Interval.of(c0, d1).simplify()).toEqual(new Interval(2));
+  });
+
+  test("describe methods", () => {
+    interface TestCase {
+      lhs: Pitch;
+      rhs: Pitch;
+      method: keyof Interval;
+      expected: boolean;
+    }
+
+    const testCases: Array<TestCase> = [
+      { lhs: c0, rhs: c0, method: "isMajor", expected: false },
+      { lhs: c0, rhs: e0, method: "isMajor", expected: true },
+      { lhs: c0, rhs: es0, method: "isMajor", expected: false },
+      { lhs: c0, rhs: c0, method: "isMinor", expected: false },
+      { lhs: c0, rhs: e0, method: "isMinor", expected: false },
+      { lhs: c0, rhs: es0, method: "isMinor", expected: true },
+      { lhs: c0, rhs: c0, method: "isUnison", expected: true },
+      { lhs: c0, rhs: e0, method: "isUnison", expected: false },
+      { lhs: c0, rhs: es0, method: "isUnison", expected: false }
+    ];
+
+    for (const testCase of testCases) {
+      expect(Interval.of(testCase.lhs, testCase.rhs)[testCase.method]()).toBe(
+        testCase.expected
+      );
+    }
   });
 });
--- src/domain/models/interval.ts
+++ src/domain/models/interval.ts
@@ -1,14 +1,42 @@
 import { Pitch } from "./pitch";
 
+enum Distance {
+  PERFECT_UNISON = 0,
+  MINOR_SECOND,
+  MAJOR_SECOND,
+  MINOR_THIRD,
+  MAJOR_THIRD,
+  PERFECT_FOURTH,
+  DIMINISHED_FIFTH,
+  PERFECT_FIFTH,
+  MINOR_SIXTH,
+  MAJOR_SIXTH,
+  MINOR_SEVENTH,
+  MAJOR_SEVENTH,
+  PERFECT_OCTAVE
+}
+
 export class Interval {
   static OCTAVE = 11;
   static of(a: Pitch, b: Pitch): Interval {
     return new Interval(Math.abs(a.index - b.index));
   }
 
-  constructor(private value: number) {}
+  constructor(private distance: number) {}
 
   simplify(): Interval {
-    return new Interval(this.value % Interval.OCTAVE);
+    return new Interval(this.distance % Interval.OCTAVE);
+  }
+
+  isUnison(): boolean {
+    return this.distance === Distance.PERFECT_UNISON;
+  }
+
+  isMajor(): boolean {
+    return this.distance === Distance.MAJOR_THIRD;
+  }
+
+  isMinor(): boolean {
+    return this.distance === Distance.MINOR_THIRD;
   }
 }
