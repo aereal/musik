86879a1499395c28f743fdca9bcc34b000dde59e
aereal <aereal@aereal.org>
2019-08-31 10:38:55 +0900

    readonly

--- src/array/zip.ts
+++ src/array/zip.ts
@@ -1,11 +1,13 @@
 export const zipWith = <X, Y, U, V>(
   f: (x: X, y: Y) => [U, V],
-  xs: X[],
-  ys: Y[]
-): Array<[U, V]> =>
+  xs: readonly X[],
+  ys: readonly Y[]
+): ReadonlyArray<[U, V]> =>
   xs.length <= ys.length
     ? xs.map((x, i) => f(x, ys[i]))
     : ys.map((y, i) => f(xs[i], y));
 
-export const zip = <X, Y>(xs: X[], ys: Y[]): Array<[X, Y]> =>
-  zipWith((x, y) => [x, y], xs, ys);
+export const zip = <X, Y>(
+  xs: readonly X[],
+  ys: readonly Y[]
+): ReadonlyArray<[X, Y]> => zipWith((x, y) => [x, y], xs, ys);
